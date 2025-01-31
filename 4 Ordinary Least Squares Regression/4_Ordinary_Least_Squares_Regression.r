0d4346af4a6817a8c2e0f764548d8e5540e42c3d
Thomas Bryan Smith <61707281+thomasbryansmith@users.noreply.github.com>
2025-01-30 23:03:54 -0600

    Create 4_Ordinary_Least_Squares_Regression.r

---
+++ 4 Ordinary Least Squares Regression/4_Ordinary_Least_Squares_Regression.r
@@ -0,0 +1,16 @@
+# Title: Introducing R
+# Subtitle: CJ 702 Advanced Criminal Justice Statistics
+# Author: Thomas Bryan Smith <tbsmit10@olemiss.edu>
+# GitHub: thomasbryansmith/CJStatsOM
+#
+# Released under MIT License
+# https://github.com/thomasbryansmith/CJStatsOM?tab=MIT-1-ov-file
+
+############################################################################## #
+###                     1. Setting up your environment                      ====
+############################################################################## #
+## ---- setup
+
+## ---- end_setup
+# ============================================================================ #
+# ============================================================================ #
